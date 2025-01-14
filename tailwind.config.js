14a14d6f58105f7df09d2b039e3334e21b008826
andre-vg <avictorg@gmail.com>
2025-01-13 22:16:03 -0300

    Test for build error in Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,22 @@ import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  serverModuleFormat: "cjs",
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: [
-  				'var(--font-sans)'
-  			],
-  			heading: [
-  				'var(--font-heading)'
-  			],
-  			body: [
-  				'var(--font-body)'
-  			],
-  			sub: [
-  				'var(--font-sub)'
-  			],
-  			mono: [
-  				'var(--font-mono)'
-  			]
-  		},
-  	}
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        heading: ["var(--font-heading)"],
+        body: ["var(--font-body)"],
+        sub: ["var(--font-sub)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
   },
   darkMode: ["class"],
   plugins: [nextui(), require("tailwindcss-animate")],
