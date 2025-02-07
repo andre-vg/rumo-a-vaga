7101ca84cda88de0ffe32aa1b9473d1f97bfd03e
André Gonçalves <andre.goncalves@grupoicts.com.br>
2025-02-07 14:57:24 -0300

    ✨ Update Estudos page title and add subtitle for improved user guidance

--- app/(auth)/estudos/page.tsx
+++ app/(auth)/estudos/page.tsx
@@ -1,11 +1,16 @@
 import TableEstudos from "@/components/Estudos/tableEstudos";
-import { title } from "@/components/primitives";
+import { subtitle, title } from "@/components/primitives";
 import React from "react";
 
 export default function PageEstudos() {
   return (
     <div>
-      <h1 className={title()}>Em desenvolvimento!!</h1>
+      <h1 className={title({ size: "lg" })}>Histórico de estudos</h1>
+      <h3 className={subtitle({className:"!w-3/4"})}>
+        Abaixo temos uma tabela mostrando seus estudos anteriores, caso tenha
+        cometido algum erro ao salvar, você pode editar e corrigí-los, ou até
+        mesmo apagar...
+      </h3>
       <TableEstudos />
     </div>
   );
