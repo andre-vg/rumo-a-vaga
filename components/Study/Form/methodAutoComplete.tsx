67987dd6ff862705a954552147e2c501260e5766
André Gonçalves <andre.goncalves@grupoicts.com.br>
2025-02-07 14:58:04 -0300

    ✨ Refactor MethodAutoComplete component to accept defaultSelectedKey prop and enhance item rendering

--- components/Study/Form/methodAutoComplete.tsx
+++ components/Study/Form/methodAutoComplete.tsx
@@ -1,20 +1,56 @@
 import { Autocomplete, AutocompleteItem } from "@heroui/autocomplete";
 import React from "react";
 
-export default function MethodAutoComplete() {
+export default function MethodAutoComplete({
+  defaultSelectedKey,
+}: {
+  defaultSelectedKey?: string | null;
+}) {
   return (
-    <Autocomplete label="Método de estudo" name="method" isRequired>
-      <AutocompleteItem value="Videoaula">Videoaula</AutocompleteItem>
-      <AutocompleteItem value="Livro">Livro</AutocompleteItem>
-      <AutocompleteItem value="Lei Seca">Lei Seca</AutocompleteItem>
-      <AutocompleteItem value="Resumo">Resumo</AutocompleteItem>
-      <AutocompleteItem value="Revisão">Revisão</AutocompleteItem>
-      <AutocompleteItem value="Exercícios">Exercícios</AutocompleteItem>
-      <AutocompleteItem value="Simulado">Simulado</AutocompleteItem>
-      <AutocompleteItem value="Aula presencial">
+    <Autocomplete
+      defaultSelectedKey={
+        defaultSelectedKey as
+          | "Videoaula"
+          | "Livro"
+          | "Lei Seca"
+          | "Resumo"
+          | "Revisão"
+          | "Exercícios"
+          | "Simulado"
+          | "Aula presencial"
+          | "PDF"
+      }
+      label="Método de estudo"
+      name="method"
+      isRequired
+    >
+      <AutocompleteItem key="Videoaula" value="Videoaula">
+        Videoaula
+      </AutocompleteItem>
+      <AutocompleteItem key="Livro" value="Livro">
+        Livro
+      </AutocompleteItem>
+      <AutocompleteItem key="Lei Seca" value="Lei Seca">
+        Lei Seca
+      </AutocompleteItem>
+      <AutocompleteItem key="Resumo" value="Resumo">
+        Resumo
+      </AutocompleteItem>
+      <AutocompleteItem key="Revisão" value="Revisão">
+        Revisão
+      </AutocompleteItem>
+      <AutocompleteItem key="Exercícios" value="Exercícios">
+        Exercícios
+      </AutocompleteItem>
+      <AutocompleteItem key="Simulado" value="Simulado">
+        Simulado
+      </AutocompleteItem>
+      <AutocompleteItem key="Aula presencial" value="Aula presencial">
         Aula presencial
       </AutocompleteItem>
-      <AutocompleteItem value="PDF">PDF</AutocompleteItem>
+      <AutocompleteItem key="PDF" value="PDF">
+        PDF
+      </AutocompleteItem>
     </Autocomplete>
   );
 }
