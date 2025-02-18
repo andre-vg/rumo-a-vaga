7bb79d427c7a67460273ab73e2a9d665b3284c91
André Gonçalves <andre.goncalves@rybena.com.br>
2025-02-17 21:22:53 -0300

    ✨ Enhance QuestionChart component to include percentage calculation and improve tooltip display

--- components/Estatisticas/Charts/questionChart.tsx
+++ components/Estatisticas/Charts/questionChart.tsx
@@ -27,7 +27,7 @@ export function QuestionChart({
   chartData: Database["public"]["Tables"]["Study"]["Row"][] | undefined;
 }) {
   const transformChartData = (
-    chartData: Database["public"]["Tables"]["Study"]["Row"][],
+    chartData: Database["public"]["Tables"]["Study"]["Row"][]
   ) => {
     return chartData.reduce(
       (acc, current) => {
@@ -36,19 +36,30 @@ export function QuestionChart({
         if (acc[date]) {
           acc[date].questions += current.questions ?? 0;
           acc[date].correctQuestions += current.correctQuestions ?? 0;
+          acc[date].percentage = Math.round(
+            (acc[date].correctQuestions / acc[date].questions) * 100
+          );
         } else {
           acc[date] = {
             date: date.split("-").reverse().join("/"),
             questions: current.questions ?? 0,
             correctQuestions: current.correctQuestions ?? 0,
+            percentage: Math.round(
+              ((current.correctQuestions ?? 0) / (current.questions ?? 1)) * 100
+            ),
           };
         }
         return acc;
       },
       {} as Record<
         string,
-        { date: string; questions: number; correctQuestions: number }
-      >,
+        {
+          date: string;
+          questions: number;
+          correctQuestions: number;
+          percentage: number;
+        }
+      >
     );
   };
 
@@ -68,16 +79,49 @@ export function QuestionChart({
               tickMargin={10}
               axisLine={false}
             />
-            <ChartTooltip content={<ChartTooltipContent />} />
+            <ChartTooltip
+              content={
+                <ChartTooltipContent
+                  hideLabel
+                  className="w-[180px]"
+                  formatter={(value, name, item, index) => (
+                    <>
+                      <div
+                        className="h-2.5 w-2.5 shrink-0 rounded-[2px]"
+                        style={{
+                          backgroundColor: item.color as string,
+                        }}
+                      />
+                      {chartConfig[name as keyof typeof chartConfig]?.label ||
+                        name}
+                      <div className="ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
+                        {value}
+                      </div>
+                      {/* Add this after the last item */}
+                      {index === 1 && (
+                        <div className="mt-1.5 flex basis-full items-center border-t pt-1.5 text-xs font-medium text-foreground">
+                          Taxa de acertos
+                          <div className="ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
+                            {item.payload.percentage}%
+                          </div>
+                        </div>
+                      )}
+                    </>
+                  )}
+                />
+              }
+              cursor={false}
+              defaultIndex={1}
+            />{" "}
             <ChartLegend content={<ChartLegendContent />} />
-            <Bar
-              dataKey="correctQuestions"
-              fill="hsl(var(--heroui-success))"
-              radius={8}
-            />
             <Bar
               dataKey="questions"
               fill="hsl(var(--heroui-primary))"
+              radius={8}
+            />
+            <Bar
+              dataKey="correctQuestions"
+              fill="hsl(var(--heroui-success))"
               radius={8}
             />
           </BarChart>
