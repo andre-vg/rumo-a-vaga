899de6e15e4da52becd400f5ba90d0a8093cfb10
andre-vg <avictorg@gmail.com>
2025-04-03 19:52:39 -0300

    ✨ Refactor middleware to improve URL redirection logic

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { auth } from "@/auth";
 
 export default auth((req) => {
   if (!req.auth && req.nextUrl.pathname !== "/") {
-    const newUrl = new URL("/", req.nextUrl.origin);
+    const newUrl = new URL("/", req.nextUrl.origin); 
 
     return Response.redirect(newUrl);
   }
